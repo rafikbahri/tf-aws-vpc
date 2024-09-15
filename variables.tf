fbba0e72443e422dc78d69a2c0ca8995230b8853
Rafik Bahri <rafikbahri.insat@gmail.com>
2024-09-15 13:29:17 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "instance_tenancy" {
+  description = "Tenancy for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "name" {
+  description = "VPC name"
+  type        = string
+  default     = "main"
+}
+
+variable "tags" {
+  description = "VPC tags"
+  type        = map(string)
+  default     = {}
+}
+
+variable "create_igw" {
+  description = "Create an Internet Gateway for public subnets"
+  type        = bool
+  default     = true
+}
